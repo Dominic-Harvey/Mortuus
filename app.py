aba301034151e7839ff78e2b617ddde8bc43dd2c
Dominic-Harvey <66623504+Dominic-Harvey@users.noreply.github.com>
2021-01-30 13:42:33 +0000

    create database for users

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+import sqlite3
 
 app = Flask(__name__)
 
+conn = sqlite3.connect('mortuus.db')
+c = conn.cursor()
 
-@app.route("/")
+c.execute("""CREATE TABLE users (
+    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    'username' TEXT NOT NULL,
+    'hash' TEXT NOT NULL   
+)""")
+
+conn.commit()
+conn.close()
+
+
+@app.route('/')
 def index():
-    return "Hello, World test"
+    return 'Hello, World test'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
